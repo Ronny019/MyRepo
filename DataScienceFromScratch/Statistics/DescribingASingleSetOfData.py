b4ba34f4645ed8b4e22ece3dd1d42274cb9e1361
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-01-08 20:04:09 +0530

    Central Tendency code

--- DataScienceFromScratch/Statistics/DescribingASingleSetOfData.py
+++ DataScienceFromScratch/Statistics/DescribingASingleSetOfData.py
@@ -1,5 +1,7 @@
+from __future__ import division
 from collections import Counter
 from matplotlib import pyplot as plt
+
 num_friends = [100, 49, 41, 40, 25,
 # ... and lots more
 ]
@@ -24,3 +26,44 @@ sorted_values = sorted(num_friends)
 smallest_value = sorted_values[0] # 1
 second_smallest_value = sorted_values[1] # 1
 second_largest_value = sorted_values[-2] # 49
+
+# this isn't right if you don't from __future__ import division
+def mean(x):
+    return sum(x) / len(x)
+print(mean(num_friends)) # 7.333333
+
+def median(v):
+    """finds the 'middle-most' value of v"""
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n // 2
+    if n % 2 == 1:
+        # if odd, return the middle value
+        return sorted_v[midpoint]
+    else:
+        # if even, return the average of the middle values
+        lo = midpoint - 1
+        hi = midpoint
+        return (sorted_v[lo] + sorted_v[hi]) / 2
+
+print(median(num_friends)) # 6.0
+
+def quantile(x, p):
+    """returns the pth-percentile value in x"""
+    p_index = int(p * len(x))
+    return sorted(x)[p_index]
+
+
+print(quantile(num_friends, 0.10)) # 1
+print(quantile(num_friends, 0.25)) # 3
+print(quantile(num_friends, 0.75)) # 9
+print(quantile(num_friends, 0.90)) # 13
+
+def mode(x):
+    """returns a list, might be more than one mode"""
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.items()
+    if count == max_count]
+
+print(mode(num_friends)) # 1 and 6
