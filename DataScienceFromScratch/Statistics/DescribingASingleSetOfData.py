8d132c78b4d9fb43681ff29b40705c0235283bf9
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-01-08 22:57:23 +0530

    Dispersion code added

--- DataScienceFromScratch/Statistics/DescribingASingleSetOfData.py
+++ DataScienceFromScratch/Statistics/DescribingASingleSetOfData.py
@@ -1,7 +1,8 @@
 from __future__ import division
+import Vectors as vec
 from collections import Counter
 from matplotlib import pyplot as plt
-
+import math
 num_friends = [100, 49, 41, 40, 25,
 # ... and lots more
 ]
@@ -67,3 +68,31 @@ def mode(x):
     if count == max_count]
 
 print(mode(num_friends)) # 1 and 6
+
+# "range" already means something in Python, so we'll use a different name
+def data_range(x):
+    return max(x) - min(x)
+print(data_range(num_friends)) # 99
+
+def de_mean(x):
+    """translate x by subtracting its mean (so the result has mean 0)"""
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+def variance(x):
+    """assumes x has at least two elements"""
+    n = len(x)
+    deviations = de_mean(x)
+    return vec.sum_of_squares(deviations) / (n - 1)
+
+print(variance(num_friends)) # 81.54
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+print(standard_deviation(num_friends)) # 9.03
+
+def interquartile_range(x):
+    return quantile(x, 0.75) - quantile(x, 0.25)
+
+print(interquartile_range(num_friends)) # 6
