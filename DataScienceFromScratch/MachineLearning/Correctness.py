0329d76e75235f1e0e50fcb7d2097beca10b5a21
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-02-09 12:26:52 +0530

    Added MachineLearning project, OverfittingAndUnderfitting.py and Correctness.py

---
+++ DataScienceFromScratch/MachineLearning/Correctness.py
@@ -0,0 +1,23 @@
+#                leukemia    no leukemia     total
+#“Luke”           70          4,930           5,000
+#not “Luke”      13,930      981,070         995,000
+#total           14,000      986,000         1,000,000
+
+def accuracy(tp, fp, fn, tn):
+    correct = tp + tn
+    total = tp + fp + fn + tn
+    return correct / total
+
+print(accuracy(70, 4930, 13930, 981070)) # 0.98114
+
+def precision(tp, fp, fn, tn):
+    return tp / (tp + fp)
+
+print(precision(70, 4930, 13930, 981070)) # 0.014
+
+def recall(tp, fp, fn, tn):
+    return tp / (tp + fn)
+
+print(recall(70, 4930, 13930, 981070)) # 0.005
+
+
