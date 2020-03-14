d6dcb8100b01edb8208d9c885cba6ca7b65c57f8
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-03-14 16:08:39 +0530

    Added more code to BetweennessCentrality.py

--- DataScienceFromScratch/NetworkAnalysis/BetweennessCentrality.py
+++ DataScienceFromScratch/NetworkAnalysis/BetweennessCentrality.py
@@ -22,3 +22,87 @@ for i, j in friendships:
     # this works because users[i] is the user whose id is i
     users[i]["friends"].append(users[j]) # add i as a friend of j
     users[j]["friends"].append(users[i]) # add j as a friend of i
+
+
+from collections import deque
+
+def shortest_paths_from(from_user):
+
+    # a dictionary from "user_id" to *all* shortest paths to that user
+    shortest_paths_to = { from_user["id"] : [[]] }
+
+    # a queue of (previous user, next user) that we need to check.
+    # starts out with all pairs (from_user, friend_of_from_user)
+    frontier = deque((from_user, friend)
+                    for friend in from_user["friends"])
+
+    # keep going until we empty the queue
+    while frontier:
+        prev_user, user = frontier.popleft() # remove the user who's
+        user_id = user["id"] # first in the queue
+
+        # because of the way we're adding to the queue,
+        # necessarily we already know some shortest paths to prev_user
+        paths_to_prev_user = shortest_paths_to[prev_user["id"]]
+        new_paths_to_user = [path + [user_id] for path in paths_to_prev_user]
+
+        # it's possible we already know a shortest path
+        old_paths_to_user = shortest_paths_to.get(user_id, [])
+
+        # what's the shortest path to here that we've seen so far?
+        if old_paths_to_user:
+            min_path_length = len(old_paths_to_user[0])
+        else:
+            min_path_length = float('inf')
+
+        # only keep paths that aren't too long and are actually new
+        new_paths_to_user = [path
+                            for path in new_paths_to_user
+                            if len(path) <= min_path_length
+                            and path not in old_paths_to_user]
+
+        shortest_paths_to[user_id] = old_paths_to_user + new_paths_to_user
+
+        # add never-seen neighbors to the frontier
+        frontier.extend((user, friend)
+                        for friend in user["friends"]
+                        if friend["id"] not in shortest_paths_to)
+
+    return shortest_paths_to
+
+print(shortest_paths_from(users[0]))
+
+for user in users:
+    user["shortest_paths"] = shortest_paths_from(user)
+
+
+for user in users:
+    user["betweenness_centrality"] = 0.0
+
+for source in users:
+    source_id = source["id"]
+    for target_id, paths in source["shortest_paths"].items():
+        if source_id < target_id: # don't double count
+            num_paths = len(paths) # how many shortest paths?
+            contrib = 1 / num_paths # contribution to centrality
+            for path in paths:
+                for id in path:
+                    if id not in [source_id, target_id]:
+                        users[id]["betweenness_centrality"] += contrib
+
+
+for user in users:
+    print(user["id"],user["betweenness_centrality"])
+
+
+def farness(user):
+    """the sum of the lengths of the shortest paths to each other user"""
+    return sum(len(paths[0])
+                for paths in user["shortest_paths"].values())
+
+
+for user in users:
+    user["closeness_centrality"] = 1 / farness(user)
+
+for user in users:
+    print(user["id"],user["closeness_centrality"])
