8110d73e4a76a0187a235c8eb68cf7e6cf27d1df
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-05-08 02:39:30 +0530

    K Means Clustering

---
+++ Udemy/MachineLearning_A_Z/Clustering/KMeansClustering/KMeans_MyImp.py
@@ -0,0 +1,18 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, 1:].values
+
+print(X)
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
