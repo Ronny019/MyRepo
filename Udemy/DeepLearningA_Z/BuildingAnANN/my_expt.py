b353950ca20b515e651dda8e1d2f6f73283f7c97
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-05-25 23:38:24 +0530

    BuildingCNN

--- Udemy/DeepLearningA_Z/BuildingAnANN/my_expt.py
+++ Udemy/DeepLearningA_Z/BuildingAnANN/my_expt.py
@@ -27,68 +27,57 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#import keras
-#from keras.models import Sequential
-#from keras.layers import Dense
-#from keras.layers import Dropout
-
-## Initialising the ANN
-#classifier = Sequential()
-
-## Adding the input layer and the first hidden layer
-#classifier.add(Dense(units = 120, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
-## classifier.add(Dropout(p = 0.1))
-
-## Adding the second hidden layer
-#classifier.add(Dense(units = 60, kernel_initializer = 'uniform', activation = 'relu'))
-## classifier.add(Dropout(p = 0.1))
-
-## Adding the second hidden layer
-#classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
-## classifier.add(Dropout(p = 0.1))
-
-
-## Adding the output layer
-#classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-
-## Compiling the ANN
-#classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-## Fitting the ANN to the Training set
-#classifier.fit(X_train, y_train, batch_size = 25, epochs = 100)
-
-## Part 3 - Making predictions and evaluating the model
-
-## Predicting the Test set results
-#y_pred = classifier.predict(X_test)
-#y_pred = (y_pred > 0.5)
-
-## Making the Confusion Matrix
-#from sklearn.metrics import confusion_matrix
-#cm = confusion_matrix(y_test, y_pred)
-#print(cm)
-
-
-# Evaluating the ANN
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import cross_val_score
+import keras
 from keras.models import Sequential
 from keras.layers import Dense
-def build_classifier():
-    classifier = Sequential()
-    classifier.add(Dense(units = 120, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
-    classifier.add(Dense(units = 60, kernel_initializer = 'uniform', activation = 'relu'))
-    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
-    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-    classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
-    return classifier
-classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-print(accuracies)
-mean = accuracies.mean()
-print(mean)
-variance = accuracies.std()
-print(variance)
+from keras.layers import Dropout
+
+# Initialising the ANN
+classifier = Sequential()
+
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 25, epochs = 100)
+
+# Part 3 - Making predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test,y_pred)
+print(cm)
+print(acc)
+
+## Evaluating the ANN
+#from keras.wrappers.scikit_learn import KerasClassifier
+#from sklearn.model_selection import cross_val_score
+#from keras.models import Sequential
+#from keras.layers import Dense
+#def build_classifier():
+#    classifier = Sequential()
+#    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+#    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+#    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+#    classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
+#    return classifier
+#classifier = KerasClassifier(build_fn = build_classifier, batch_size = 25, epochs = 100)
+#accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+#print(accuracies)
+#mean = accuracies.mean()
+#print(mean)
+#variance = accuracies.std()
+#print(variance)
 
 
 #from keras.wrappers.scikit_learn import KerasClassifier
