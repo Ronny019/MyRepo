c6715ac21f74be9ea47e13af995bf1a36e913f74
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-07-05 16:15:26 +0530

    UpAndRunningWithTF

--- MachineLearning_ScikitLearnAndTensorflow/TensorFlow/UpAndRunningWithTF/UpAndRunningWithTF.py
+++ MachineLearning_ScikitLearnAndTensorflow/TensorFlow/UpAndRunningWithTF/UpAndRunningWithTF.py
@@ -48,3 +48,410 @@ with graph.as_default():
 x2.graph is graph
 
 x2.graph is tf.get_default_graph()
+
+w = tf.constant(3)
+x = w + 2
+y = x + 5
+z = x * 3
+
+with tf.Session() as sess:
+    print(y.eval())  # 10
+    print(z.eval())  # 15
+
+
+
+with tf.Session() as sess:
+    y_val, z_val = sess.run([y, z])
+    print(y_val)  # 10
+    print(z_val)  # 15
+
+
+#Linear regression
+#Using Normal Equation
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+tf.reset_default_graph()
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+
+with tf.Session() as sess:
+    theta_value = theta.eval()
+
+
+
+#Using Batch Gradient Descent
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+
+print(scaled_housing_data_plus_bias.mean(axis=0))
+print(scaled_housing_data_plus_bias.mean(axis=1))
+print(scaled_housing_data_plus_bias.mean())
+print(scaled_housing_data_plus_bias.shape)
+
+#Manually computing the gradients
+tf.reset_default_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients = 2/m * tf.matmul(tf.transpose(X), error)
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+
+
+#Using a GradientDescentOptimizer
+tf.reset_default_graph()
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)
+
+#Placeholder nodes
+tf.reset_default_graph()
+A = tf.placeholder(tf.float32, shape=(None, 3))
+B = A + 5
+with tf.Session() as sess:
+    B_val_1 = B.eval(feed_dict={A: [[1, 2, 3]]})
+    B_val_2 = B.eval(feed_dict={A: [[4, 5, 6], [7, 8, 9]]})
+
+print(B_val_1)
+print(B_val_2)
+
+#Mini-batch Gradient Descent
+n_epochs = 1000
+learning_rate = 0.01
+
+tf.reset_default_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+n_epochs = 10
+
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)  # not shown in the book
+    indices = np.random.randint(m, size=batch_size)  # not shown
+    X_batch = scaled_housing_data_plus_bias[indices] # not shown
+    y_batch = housing.target.reshape(-1, 1)[indices] # not shown
+    return X_batch, y_batch
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()
+
+
+#Saving and restoring a model
+
+tf.reset_default_graph()
+
+n_epochs = 1000                                                                       # not shown in the book
+learning_rate = 0.01                                                                  # not shown
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")            # not shown
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")            # not shown
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")                                      # not shown
+error = y_pred - y                                                                    # not shown
+mse = tf.reduce_mean(tf.square(error), name="mse")                                    # not shown
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)            # not shown
+training_op = optimizer.minimize(mse)                                                 # not shown
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())                                # not shown
+            save_path = saver.save(sess, "./tmp/my_model.ckpt")
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+    save_path = saver.save(sess, "./tmp/my_model_final.ckpt")
+
+
+with tf.Session() as sess:
+    saver.restore(sess, "/tmp/my_model_final.ckpt")
+    best_theta_restored = theta.eval() # not shown in the book
+
+np.allclose(best_theta, best_theta_restored)
+
+saver = tf.train.Saver({"weights": theta})
+
+
+tf.reset_default_graph()
+# notice that we start with an empty graph.
+
+saver = tf.train.import_meta_graph("/tmp/my_model_final.ckpt.meta")  # this loads the graph structure
+theta = tf.get_default_graph().get_tensor_by_name("theta:0") # not shown in the book
+
+with tf.Session() as sess:
+    saver.restore(sess, "/tmp/my_model_final.ckpt")  # this restores the graph's state
+    best_theta_restored = theta.eval() # not shown in the book
+
+np.allclose(best_theta, best_theta_restored)
+
+
+tf.reset_default_graph()
+
+from datetime import datetime
+
+now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+root_logdir = "tf_logs"
+logdir = "{}/run-{}/".format(root_logdir, now)
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+mse_summary = tf.summary.scalar('MSE', mse)
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+n_epochs = 10
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+with tf.Session() as sess:                                                        # not shown in the book
+    sess.run(init)                                                                # not shown
+
+    for epoch in range(n_epochs):                                                 # not shown
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            if batch_index % 10 == 0:
+                summary_str = mse_summary.eval(feed_dict={X: X_batch, y: y_batch})
+                step = epoch * n_batches + batch_index
+                file_writer.add_summary(summary_str, step)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()                                                     # not shown
+
+file_writer.close()
+
+best_theta
+
+tf.reset_default_graph()
+
+now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+root_logdir = "tf_logs"
+logdir = "{}/run-{}/".format(root_logdir, now)
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+
+with tf.name_scope("loss") as scope:
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+mse_summary = tf.summary.scalar('MSE', mse)
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+n_epochs = 10
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            if batch_index % 10 == 0:
+                summary_str = mse_summary.eval(feed_dict={X: X_batch, y: y_batch})
+                step = epoch * n_batches + batch_index
+                file_writer.add_summary(summary_str, step)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()
+
+file_writer.flush()
+file_writer.close()
+print("Best theta:")
+print(best_theta)
+
+
+tf.reset_default_graph()
+
+def relu(X):
+    w_shape = (int(X.get_shape()[1]), 1)
+    w = tf.Variable(tf.random_normal(w_shape), name="weights")
+    b = tf.Variable(0.0, name="bias")
+    z = tf.add(tf.matmul(X, w), b, name="z")
+    return tf.maximum(z, 0., name="relu")
+
+n_features = 3
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = [relu(X) for i in range(5)]
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu1", tf.get_default_graph())
+
+
+tf.reset_default_graph()
+
+def relu(X):
+    with tf.name_scope("relu"):
+        w_shape = (int(X.get_shape()[1]), 1)                          # not shown in the book
+        w = tf.Variable(tf.random_normal(w_shape), name="weights")    # not shown
+        b = tf.Variable(0.0, name="bias")                             # not shown
+        z = tf.add(tf.matmul(X, w), b, name="z")                      # not shown
+        return tf.maximum(z, 0., name="max")                          # not shown
+
+n_features = 3
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = [relu(X) for i in range(5)]
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu2", tf.get_default_graph())
+file_writer.close()
+
+
+tf.reset_default_graph()
+def relu(X):
+    with tf.variable_scope("relu", reuse=True):
+        threshold = tf.get_variable("threshold")
+        w_shape = int(X.get_shape()[1]), 1                          # not shown
+        w = tf.Variable(tf.random_normal(w_shape), name="weights")  # not shown
+        b = tf.Variable(0.0, name="bias")                           # not shown
+        z = tf.add(tf.matmul(X, w), b, name="z")                    # not shown
+        return tf.maximum(z, threshold, name="max")
+
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+with tf.variable_scope("relu"):
+    threshold = tf.get_variable("threshold", shape=(),
+                                initializer=tf.constant_initializer(0.0))
+relus = [relu(X) for relu_index in range(5)]
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu6", tf.get_default_graph())
+file_writer.close()
+
+tf.reset_default_graph()
+
+def relu(X):
+    with tf.variable_scope("relu"):
+        threshold = tf.get_variable("threshold", shape=(), initializer=tf.constant_initializer(0.0))
+        w_shape = (int(X.get_shape()[1]), 1)
+        w = tf.Variable(tf.random_normal(w_shape), name="weights")
+        b = tf.Variable(0.0, name="bias")
+        z = tf.add(tf.matmul(X, w), b, name="z")
+        return tf.maximum(z, threshold, name="max")
+
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+with tf.variable_scope("", default_name="") as scope:
+    first_relu = relu(X)     # create the shared variable
+    scope.reuse_variables()  # then reuse it
+    relus = [first_relu] + [relu(X) for i in range(4)]
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu8", tf.get_default_graph())
+file_writer.close()
+
+tf.reset_default_graph()
+
+def relu(X):
+    threshold = tf.get_variable("threshold", shape=(),
+                                initializer=tf.constant_initializer(0.0))
+    w_shape = (int(X.get_shape()[1]), 1)                        # not shown in the book
+    w = tf.Variable(tf.random_normal(w_shape), name="weights")  # not shown
+    b = tf.Variable(0.0, name="bias")                           # not shown
+    z = tf.add(tf.matmul(X, w), b, name="z")                    # not shown
+    return tf.maximum(z, threshold, name="max")
+
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = []
+for relu_index in range(5):
+    with tf.variable_scope("relu", reuse=(relu_index >= 1)) as scope:
+        relus.append(relu(X))
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu9", tf.get_default_graph())
+file_writer.close()
