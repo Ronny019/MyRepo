00ea219d24b47e26e7a954b0b8e69d8eed881c2d
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2020-08-02 20:18:49 +0530

    Added CNN chapter files

---
+++ MachineLearning_ScikitLearnAndTensorflow/TensorFlow/ConvolutionalNeuralNetworks/ConvolutionalNeuralNetworks.py
@@ -0,0 +1,110 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+from io import open
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+# To plot pretty figures
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "cnn"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+#A couple utility functions to plot grayscale and RGB images:
+
+
+def plot_image(image):
+    plt.imshow(image, cmap="gray", interpolation="nearest")
+    plt.axis("off")
+
+def plot_color_image(image):
+    plt.imshow(image.astype(np.uint8),interpolation="nearest")
+    plt.axis("off")
+#And of course we will need TensorFlow:
+
+import tensorflow as tf
+import numpy as np
+from sklearn.datasets import load_sample_image
+from sklearn.datasets import load_sample_images
+
+# Load sample images
+china = load_sample_image("china.jpg")
+flower = load_sample_image("flower.jpg")
+dataset = np.array([china, flower], dtype=np.float32)
+batch_size, height, width, channels = dataset.shape
+
+# Create 2 filters
+filters = np.zeros(shape=(7, 7, channels, 2), dtype=np.float32)
+filters[:, 3, :, 0] = 1  # vertical line
+filters[3, :, :, 1] = 1  # horizontal line
+
+# Create a graph with input X plus a convolutional layer applying the 2 filters
+X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+convolution = tf.nn.conv2d(X, filters, strides=[1,2,2,1], padding="SAME")
+
+with tf.Session() as sess:
+    output = sess.run(convolution, feed_dict={X: dataset})
+
+plt.imshow(output[0, :, :, 1], cmap="gray") # plot 1st image's 2nd feature map
+plt.show()
+
+for image_index in (0, 1):
+    for feature_map_index in (0, 1):
+        plot_image(output[image_index, :, :, feature_map_index])
+        plt.show()
+
+reset_graph()
+
+X = tf.placeholder(shape=(None, height, width, channels), dtype=tf.float32)
+conv = tf.layers.conv2d(X, filters=2, kernel_size=7, strides=[2,2],
+                        padding="SAME")
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    init.run()
+    output = sess.run(conv, feed_dict={X: dataset})
+
+plt.imshow(output[0, :, :, 1], cmap="gray") # plot 1st image's 2nd feature map
+plt.show()
+
+#Pooling layer
+
+batch_size, height, width, channels = dataset.shape
+
+filters = np.zeros(shape=(7, 7, channels, 2), dtype=np.float32)
+filters[:, 3, :, 0] = 1  # vertical line
+filters[3, :, :, 1] = 1  # horizontal line
+
+X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+max_pool = tf.nn.max_pool(X, ksize=[1,2,2,1], strides=[1,2,2,1],padding="VALID")
+
+with tf.Session() as sess:
+    output = sess.run(max_pool, feed_dict={X: dataset})
+
+plot_color_image(dataset[0])
+save_fig("china_original")
+plt.show()
+    
+plot_color_image(output[0])
+save_fig("china_max_pool")
+plt.show()
