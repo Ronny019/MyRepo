6341a3d834ce3ab10a6f6e9322a0c6fe28b71037
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2021-10-21 19:04:54 +0530

    Sample Code in Flask

--- KrishNaik Tutorials/Flask Tutorials/app.py
+++ KrishNaik Tutorials/Flask Tutorials/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
+# WSGI Application
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')  # Parameter is url. Now it is homepage
 def welcome():
-    return 'Msg'
+    return "Any Message."
+
+
+@app.route('/members')  # Parameter is url. Now it is /members
+def members():
+    return "Any Message. Members"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
