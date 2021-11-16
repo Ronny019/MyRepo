28644dd633effe24ac6470b987bd8f35a2ce650b
Mainak Pal Chowdhury <mainak.pc@gmail.com>
2021-11-16 07:57:25 +0530

    Added Gradient descent

--- Pytorch Tutorial/backpropagartion_my_imp.py
+++ Pytorch Tutorial/backpropagartion_my_imp.py
@@ -1,16 +1,16 @@
 import torch
 
-x = torch.tensor(5.0)
-y = torch.tensor(25.0)
+x = torch.tensor([1,2,3,4,5], dtype=torch.float32)
+y = torch.tensor([5,9,13,17,21], dtype=torch.float32)
 
 LEARNING_RATE = 0.01
 # This is the parameter we want to optimize -> requires_grad=True
 a = torch.tensor(1.0, requires_grad=True)
 b = torch.tensor(1.0, requires_grad=True)
-for i in range(3):
+for i in range(20):
     print(f"Epoch: {i}")
     y_predicted = (a * x) + b
-    loss = (y_predicted - y) ** 2
+    loss = ((y_predicted - y) ** 2).mean()
     print(loss)
 
     loss.backward()
